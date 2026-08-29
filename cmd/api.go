24b726d219a119754ef06b81f4e5ad6040f3b2da
PauGarciaValero <pau.garcia.valero@gmail.com>
2026-08-29 19:23:46 +0200

    Refactor API structure and user routing for improved organization and clarity

---
+++ cmd/api.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"pulse/internal/database"
+	"pulse/internal/user"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type api struct {
+	router *chi.Mux
+	pool   *pgxpool.Pool
+}
+
+func newAPI(ctx context.Context) (*api, error) {
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		return nil, errors.New("DATABASE_URL environment variable is not set")
+	}
+
+	pool, err := database.Connect(ctx, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Database up")
+
+	// Dependecies
+	userRepo := user.NewRepository(pool)
+	userService := user.NewService(userRepo)
+
+	// Router
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Route("/auth", user.Router(userService))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	return &api{router: r, pool: pool}, nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -4,46 +4,21 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"os"
-	"pulse/internal/database"
-	"pulse/internal/user"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic("failed to load .env file: " + err.Error())
-	}
-
+	godotenv.Load()
 	ctx := context.Background()
 
-	dsn := os.Getenv("DATABASE_URL")
-	if dsn == "" {
-		panic("DATABASE_URL environment variable is not set")
-	}
-
-	pool, err := database.Connect(ctx, dsn)
+	app, err := newAPI(ctx)
 	if err != nil {
 		panic(err)
 	}
-	defer pool.Close()
-	log.Printf("Database up")
 
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-
-	// Routes
-	r.Route("/auth", user.Router(pool))
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("OK"))
-	})
+	defer app.pool.Close()
 
 	log.Printf("API up")
-	http.ListenAndServe(":8080", r)
-
+	http.ListenAndServe(":8080", app.router)
 }
